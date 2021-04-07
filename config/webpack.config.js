8705af62407a89705f1565ec93b15d69c155c69c
Jorge Rojas <jl.rojas@viaducto.io>
2021-04-07 11:38:57 -0500

    release webpack
    Co-authored-by: Juan Olvera <jp-olvera@users.noreply.github.com>

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,53 +1,58 @@
 const path = require("path");
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "../dist"),
-		filename: "index.js",
-		libraryTarget: "commonjs2",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules|build|dist)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env", "@babel/preset-react"],
-					},
-				},
-			},
-			{
-				test: /\.css$/,
-				exclude: /(node_modules|build)/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	externals: {
-		react: {
-			commonjs: "react",
-			commonjs2: "react",
-			amd: "React",
-			root: "React",
-		},
-		"prop-types": {
-			commonjs: "prop-types",
-			commonjs2: "prop-types",
-			amd: "PropTypes",
-			root: "PropTypes",
-		},
-	},
-	resolve: {
-		extensions: [".js", ".jsx", ".css"],
-		alias: {
-			react: path.resolve(__dirname, "./node_modules/react"),
-			"prop-types": path.resolve(__dirname, "./node_modules/prop-types"),
-			"styled-components": path.resolve(
-				__dirname,
-				"../node_modules/styled-components"
-			),
-		},
-	},
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|build|dist)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|build)/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "prop-types": {
+      commonjs: "prop-types",
+      commonjs2: "prop-types",
+      amd: "PropTypes",
+      root: "PropTypes",
+    },
+    "styled-components": {
+      commonjs: "styled-components",
+      commonjs2: "styled-components",
+      amd: "styled-components",
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "prop-types": path.resolve(__dirname, "./node_modules/prop-types"),
+      "styled-components": path.resolve(
+        __dirname,
+        "../node_modules/styled-components"
+      ),
+    },
+  },
 };
