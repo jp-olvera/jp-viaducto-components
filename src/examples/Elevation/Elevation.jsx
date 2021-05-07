6c21ebfeabb7283db4034eac927df98b7a3cfb1e
jp-olvera <jp.olvera@viaducto.io>
2021-05-07 16:58:08 -0500

    update values in elevation

--- src/examples/Elevation/Elevation.jsx
+++ src/examples/Elevation/Elevation.jsx
@@ -17,16 +17,6 @@ const Elevation = () => {
   return (
     <ConfigProvider>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {/* <Box style={{ boxShadow: '0px 1px 2px #666' }}>
-          boxShadow: 0px 1px 2px rgba(102 102 102 0.9)
-        </Box>
-        <Box style={{ boxShadow: '0px 1px 8px #666' }}>
-          boxShadow: 0px 1px 8px #666
-        </Box>
-        <Box style={{ boxShadow: '0px 1px 12px #666' }}>
-          boxShadow: 0px 1px 12px #666
-        </Box> */}
-
         <Box style={{ boxShadow: '0px 1px 4px -1px #666' }}>
           circular: 0px 1px 4px -1px 1
         </Box>
@@ -34,7 +24,7 @@ const Elevation = () => {
           circular: 0px 1px 8px -1px 0.85
         </Box>
         <Box
-          style={{ boxShadow: 'rgba(102, 102, 102, 0.75) 0px 2px 12px 1px' }}
+          style={{ boxShadow: '0px 2px 12px 1px rgba(102, 102, 102, 0.75)' }}
         >
           circular: 0px 2px 12px 1px 0.75
         </Box>
@@ -42,101 +32,101 @@ const Elevation = () => {
           top: 0px -2px 4px -3px 1
         </Box>
         <Box
-          style={{ boxShadow: '0px -3px 6px -3px rgba(102, 102, 102, 0.9)' }}
+          style={{ boxShadow: '0px -3px 6px -2px rgba(102, 102, 102, 0.9)' }}
         >
-          top: 0px -3px 6px -3px 0.85
+          top: 0px -3px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: 'rgba(102, 102, 102, 0.75) 0px -5px 8px -3px' }}
+          style={{ boxShadow: 'rgba(102, 102, 102, 0.75) 0px -5px 8px -2px' }}
         >
-          top: 0px -5px 8px -3px 0.75
+          top: 0px -5px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '2px 0px 4px -3px #666' }}>
           right: 2px 0px 4px -3px 1
         </Box>
-        <Box style={{ boxShadow: '3px 0px 6px -3px rgba(102, 102, 102, 0.9)' }}>
-          right: 3px 0px 6px -3px 0.85
+        <Box style={{ boxShadow: '3px 0px 6px -2px rgba(102, 102, 102, 0.9)' }}>
+          right: 3px 0px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: '5px 0px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '5px 0px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          right: 5px 0px 8px -3px 0.75
+          right: 5px 0px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '0px 2px 4px -3px #666' }}>
           bottom: 0px 2px 4px -3px 1
         </Box>
-        <Box style={{ boxShadow: '0px 3px 6px -3px rgba(102, 102, 102, 0.9)' }}>
-          bottom: 0px 3px 6px -3px 0.85
+        <Box style={{ boxShadow: '0px 3px 6px -2px rgba(102, 102, 102, 0.9)' }}>
+          bottom: 0px 3px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: '0px 5px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '0px 5px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          bottom: 0px 5px 8px -3px 0.75
+          bottom: 0px 5px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '-2px 0px 4px -3px #666' }}>
           left: -2px 0px 4px -3px 1
         </Box>
         <Box
-          style={{ boxShadow: '-3px 0px 6px -3px rgba(102, 102, 102, 0.9)' }}
+          style={{ boxShadow: '-3px 0px 6px -2px rgba(102, 102, 102, 0.9)' }}
         >
-          left: -3px 0px 6px -3px 0.85
+          left: -3px 0px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: '-5px 0px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '-5px 0px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          left: -5px 0px 8px -3px 0.75
+          left: -5px 0px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '2px -2px 4px -3px #666' }}>
           top-right: 2px -2px 4px -3px 1
         </Box>
         <Box
-          style={{ boxShadow: '3px -3px 6px -3px rgba(102, 102, 102, 0.9)' }}
+          style={{ boxShadow: '3px -3px 6px -2px rgba(102, 102, 102, 0.9)' }}
         >
-          top-right: 3px -3px 6px -3px 0.85
+          top-right: 3px -3px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: '5px -5px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '5px -5px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          top-right: 5px -5px 8px -3px 0.75
+          top-right: 5px -5px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '-2px -2px 4px -3px #666' }}>
           top-left: -2px -2px 4px -3px 1
         </Box>
         <Box
-          style={{ boxShadow: '-3px -3px 6px -3px rgba(102, 102, 102, 0.9)' }}
+          style={{ boxShadow: '-3px -3px 6px -2x rgba(102, 102, 102, 0.9)' }}
         >
-          top-left: -3px 3px 6px -3px 0.85
+          top-left: -3px 3px 6px -2px 0.85
         </Box>
 
         <Box
-          style={{ boxShadow: '-5px -5px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '-5px -5px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          top-left: -5px -5px 8px -3px 0.75
+          top-left: -5px -5px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '2px 2px 4px -3px #666' }}>
           bottom-right: 2px 2px 4px -3px 1
         </Box>
 
-        <Box style={{ boxShadow: '3px 3px 6px -3px rgba(102, 102, 102, 0.9)' }}>
-          bottom-right: 3px 3px 6px -3px 0.85
+        <Box style={{ boxShadow: '3px 3px 6px -2px rgba(102, 102, 102, 0.9)' }}>
+          bottom-right: 3px 3px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: '5px 5px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '5px 5px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          bottom-right: 5px 5px 8px -3px 0.75
+          bottom-right: 5px 5px 8px -2px 0.75
         </Box>
         <Box style={{ boxShadow: '-2px 2px 4px -3px #666' }}>
           bottom-left: -2px 2px 4px -3px 1
         </Box>
         <Box
-          style={{ boxShadow: '-3px 3px 6px -3px rgba(102, 102, 102, 0.9)' }}
+          style={{ boxShadow: '-3px 3px 6px -2px rgba(102, 102, 102, 0.9)' }}
         >
-          bottom-left: -3px 3px 6px -3px 0.85
+          bottom-left: -3px 3px 6px -2px 0.85
         </Box>
         <Box
-          style={{ boxShadow: '-5px 5px 8px -3px rgba(102, 102, 102, 0.75)' }}
+          style={{ boxShadow: '-5px 5px 8px -2px rgba(102, 102, 102, 0.75)' }}
         >
-          bottom-left: -5px 5px 8px -3px 0.75
+          bottom-left: -5px 5px 8px -2px 0.75
         </Box>
 
         {/* <Box style={{ filter: 'drop-shadow(0px 1px 2px #666)' }}>
