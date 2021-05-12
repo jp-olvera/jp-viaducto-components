d093e73f0130d339bd3acc8a635fed9f58802ebe
jp-olvera <jp.olvera@viaducto.io>
2021-05-12 17:54:57 -0500

    test: TypeIcon

--- src/cells/TypeIcon/TypeIcon.tsx
+++ src/cells/TypeIcon/TypeIcon.tsx
@@ -7,14 +7,14 @@ import {
 const TypeIcon = ({ type, ...rest }: any) => {
   switch (type) {
     case 'success':
-      return <Checkbox {...rest} />;
+      return <Checkbox {...rest} data-testid="checkbox"/>;
     case 'warning':
-      return <FolderWarning {...rest} />;
+      return <FolderWarning {...rest} data-testid="folderwarning" />;
     case 'danger':
-      return <Error {...rest} />;
+      return <Error {...rest} data-testid="error" />;
     case 'info':
     default:
-      return <Help {...rest} />;
+      return <Help {...rest} data-testid="help" />;
   }
 };
 
