74c2c811b0dfd0f823b6f83f212855a128da8737
Jorge Rojas <jl.rojas@viaducto.io>
2021-07-19 17:04:46 -0500

    fix: card border changes color

--- src/organisms/Card/StyledCard.ts
+++ src/organisms/Card/StyledCard.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import getElevation from '../../utils/getElevation';
 
 export const StyledNormalCard = styled.div < any > `
-  border: ${(p) => p.configuration.border};
+  border: 0.063rem solid #d9d9d9;
   background-color: ${(p) => p.configuration.colors.background};
   overflow-y: auto;
   overflow-x: hidden;
@@ -15,8 +15,8 @@ export const StyledNormalCard = styled.div < any > `
   flex-direction: column;
   justify-content: space-between;
   font-family: ${(p) => p.family || p.configuration.fontFamily};
+  border-radius: ${(p) => p.configuration.radius[p.radius]};
   .img {
-    border-radius: ${(p) => p.configuration.radius[p.radius]};
     width: 100%;
     max-height: ${({ onlyImage }) => (onlyImage ? '100%' : '11.875rem')};
     height: 100%;
