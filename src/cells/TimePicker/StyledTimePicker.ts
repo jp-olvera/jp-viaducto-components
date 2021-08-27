bffb9a10efe2a63ee0a5ffcdcd0809bb7e18217b
Jorge Rojas <jl.rojas@viaducto.io>
2021-08-27 15:40:21 -0500

    feat: TimePicker component

---
+++ src/cells/TimePicker/StyledTimePicker.ts
@@ -0,0 +1,70 @@
+import { ConfigProps } from 'ballena-types';
+import styled from 'styled-components';
+
+interface STPI {
+  timeFormat: '12h' | '24h';
+}
+export const StyledTimePicker = styled.div < STPI > `
+  transition: all 0.2s linear;
+  max-width: ${(p) => (p.timeFormat === '12h' ? '100%' : '10.7rem')};
+  max-height: 16.875rem;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: repeat(
+    ${(p) => (p.timeFormat === '12h' ? '4' : '3')},
+    1fr
+  );
+  margin: 0;
+  .time-column {
+    overflow-y: hidden;
+    overflow-x: hidden;
+    scroll-behavior: smooth;
+    &.t-hour,
+    &.t-minute {
+      border-right: 0.063rem solid #d9d9d9;
+    }
+    &.t-second {
+      ${(p) => p.timeFormat === '12h' && 'border-right: 0.063rem solid #d9d9d9;'}
+    }
+    &:hover {
+      overflow-y: auto;
+      border: none;
+    }
+  }
+`;
+
+interface OI {
+  config: ConfigProps;
+  shapeColor:
+    | 'primary'
+    | 'secondary'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'tab';
+}
+export const Option = styled.button < OI > `
+  min-width: 3rem;
+  width: 100%;
+  height: 1.75rem;
+  display: block;
+  margin: 0;
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+  transition: background-color 0.2s linear;
+  &.time-not-selected {
+    background-color: ${(p) => p.config.colors.background};
+  }
+  &.time-selected {
+    background-color: ${(p) => p.config.colors[p.shapeColor].shadow
+      || `${p.config.colors[p.shapeColor].default}53`};
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+  &:hover:not(.time-selected) {
+    background-color: #f5f5f5;
+  }
+`;
---
+++ src/cells/TimePicker/TimePicker.tsx
@@ -0,0 +1,289 @@
+import React, { useContext, useState } from 'react';
+import { Paragraph, Container } from '..';
+import { ConfigContext } from '../../providers';
+import { Option, StyledTimePicker } from './StyledTimePicker';
+
+/** onTimeSelected props */
+export interface onTimeSelectedProps {
+  /** Full time string with meridian (depends on format) */
+  fullTime?: string;
+  /** hour value format is affecting this value */
+  hour?: string;
+  /** minutes value */
+  minutes?: string;
+  /** seconds value */
+  seconds?: string;
+  /** format selected, affects the final value */
+  format?: '12h' | '24h';
+  /** set the meridian in 12h format */
+  meridian?: 'am' | 'pm';
+}
+
+/** TimePicker component */
+interface TPI {
+  /** Shape color of the details */
+  timeFormat?: '12h' | '24h';
+  /** 12h/24h time format, with/without meridian */
+  shapeColor?:
+    | 'primary'
+    | 'secondary'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'tab';
+  /** Set the time selected in key/value format. The keys are { fullTime, hour, minutes, seconds, format, meridian } and all values are string type  */
+  onTimeSelected: (time: onTimeSelectedProps) => void | undefined;
+}
+/**
+ * TimePicker component
+ * @param {string} timeFormat Shape color of the details
+ * @param {string} shapeColor 12h/24h time format, with/without meridian
+ * @param {onTimeSelectedProps} onTimeSelected Set the time selected in key/value format. The keys are { fullTime, hour, minutes, seconds, format, meridian } and all values are string type
+ */
+
+const TimePicker = ({
+  timeFormat = '24h',
+  shapeColor = 'danger',
+  onTimeSelected,
+}: TPI) => {
+  const { configuration: config } = useContext(ConfigContext);
+  const [zone, setZone] = useState('am');
+  const [timeSelected, setTimeSelected] = useState<onTimeSelectedProps>({
+    fullTime: timeFormat === '12h' ? '01:00:00 am' : '00:00:00',
+    hour: timeFormat === '12h' ? '01' : '00',
+    minutes: '00',
+    seconds: '00',
+    format: timeFormat,
+    meridian: 'am',
+  });
+  const { colors } = config;
+  const lenghtH = timeFormat === '12h' ? 12 : 24;
+  const lenghtM = 60;
+  const [selected, setSelected] = useState({
+    hour: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+  const hours: number[] = [...Array(lenghtH).keys()];
+  const minutesSeconds: number[] = [...Array(lenghtM).keys()];
+  const mer: ('am' | 'pm')[] = ['am', 'pm'];
+  const handleSelect = (num: number, opt: 'h' | 'm' | 's' = 'h') => {
+    const setOpt = () => {
+      switch (opt) {
+        case 'm':
+          return { minutes: num };
+        case 's':
+          return { seconds: num };
+        case 'h':
+        default:
+          return { hour: num };
+      }
+    };
+    setSelected((before: typeof selected) => ({
+      ...before,
+      ...setOpt(),
+    }));
+  };
+  return (
+    <StyledTimePicker timeFormat={timeFormat}>
+      <Container className='time-column t-hour' expandHorizontal expandVertical>
+        {hours.map((num: number) => (
+          <Option
+            shapeColor={shapeColor}
+            className={
+              (timeFormat === '12h' ? selected.hour + 1 : selected.hour)
+              === (timeFormat === '12h' ? num + 1 : num)
+                ? 'time-selected'
+                : 'time-not-selected'
+            }
+            config={config}
+            key={timeFormat === '12h' ? num + 1 : num}
+            onClick={() => {
+              setTimeSelected((before) => ({
+                ...before,
+                fullTime: `${
+                  num < 10 ? `0${timeFormat === '12h' ? num + 1 : num}` : num
+                }:${before.fullTime?.split(':')[1]}:${
+                  before.fullTime?.split(':')[2]
+                }`,
+                hour:
+                  num < 10
+                    ? `0${timeFormat === '12h' ? num + 1 : num}`
+                    : timeFormat === '12h'
+                      ? (num + 1).toString()
+                      : num.toString(),
+              }));
+              onTimeSelected({
+                ...timeSelected,
+                fullTime: `${
+                  num < 10 ? `0${timeFormat === '12h' ? num + 1 : num}` : num
+                }:${timeSelected.fullTime?.split(':')[1]}:${
+                  timeSelected.fullTime?.split(':')[2]
+                }`,
+                hour:
+                  num < 10
+                    ? `0${timeFormat === '12h' ? num + 1 : num}`
+                    : timeFormat === '12h'
+                      ? (num + 1).toString()
+                      : num.toString(),
+              });
+              handleSelect(num);
+            }}
+          >
+            <Paragraph
+              align='center'
+              size='sm'
+              color={
+                (timeFormat === '12h' ? selected.hour + 1 : selected.hour)
+                === (timeFormat === '12h' ? num + 1 : num)
+                  ? colors[shapeColor].text
+                  : 'dark'
+              }
+            >
+              {`${
+                (timeFormat === '12h' ? num + 1 : num) < 10
+                  ? `0${timeFormat === '12h' ? num + 1 : num}`
+                  : timeFormat === '12h'
+                    ? num + 1
+                    : num
+              }`}
+            </Paragraph>
+          </Option>
+        ))}
+      </Container>
+      <Container
+        className='time-column t-minute'
+        expandHorizontal
+        expandVertical
+      >
+        {minutesSeconds.map((num: number) => (
+          <Option
+            shapeColor={shapeColor}
+            className={
+              selected.minutes === num ? 'time-selected' : 'time-not-selected'
+            }
+            config={config}
+            key={num}
+            onClick={() => {
+              setTimeSelected((before) => ({
+                ...before,
+                fullTime: `${before.fullTime?.split(':')[0]}:${
+                  num < 10 ? `0${num}` : num
+                }:${before.fullTime?.split(':')[2]}`,
+                minutes: num < 10 ? `0${num}` : num.toString(),
+              }));
+              onTimeSelected({
+                ...timeSelected,
+                fullTime: `${timeSelected.fullTime?.split(':')[0]}:${
+                  num < 10 ? `0${num}` : num
+                }:${timeSelected.fullTime?.split(':')[2]}`,
+                minutes: num < 10 ? `0${num}` : num.toString(),
+              });
+              handleSelect(num, 'm');
+            }}
+          >
+            <Paragraph
+              align='center'
+              size='sm'
+              color={
+                selected.minutes === num ? colors[shapeColor].text : 'dark'
+              }
+            >
+              {`${num < 10 ? `0${num}` : num}`}
+            </Paragraph>
+          </Option>
+        ))}
+      </Container>
+      <Container
+        className='time-column t-second'
+        expandHorizontal
+        expandVertical
+      >
+        {minutesSeconds.map((num: number) => (
+          <Option
+            shapeColor={shapeColor}
+            className={
+              selected.seconds === num ? 'time-selected' : 'time-not-selected'
+            }
+            config={config}
+            key={num}
+            onClick={() => {
+              setTimeSelected((before) => ({
+                ...before,
+                fullTime: `${before.fullTime?.split(':')[0]}:${
+                  before.fullTime?.split(':')[1]
+                }:${num < 10 ? `0${num}` : num}${
+                  timeFormat === '12h' ? ` ${before.meridian}` : ''
+                }`,
+                seconds: num < 10 ? `0${num}` : num.toString(),
+              }));
+              onTimeSelected({
+                ...timeSelected,
+                fullTime: `${timeSelected.fullTime?.split(':')[0]}:${
+                  timeSelected.fullTime?.split(':')[1]
+                }:${num < 10 ? `0${num}` : num}${
+                  timeFormat === '12h' ? ` ${timeSelected.meridian}` : ''
+                }`,
+                seconds: num < 10 ? `0${num}` : num.toString(),
+              });
+
+              handleSelect(num, 's');
+            }}
+          >
+            <Paragraph
+              align='center'
+              size='sm'
+              color={
+                selected.seconds === num ? colors[shapeColor].text : 'dark'
+              }
+            >
+              {`${num < 10 ? `0${num}` : num}`}
+            </Paragraph>
+          </Option>
+        ))}
+      </Container>
+      {timeFormat === '12h' && (
+        <Container className='time-column' expandHorizontal expandVertical>
+          {mer.map((opt) => (
+            <Option
+              shapeColor={shapeColor}
+              className={zone === opt ? 'time-selected' : 'time-not-selected'}
+              config={config}
+              key={opt}
+              onClick={() => {
+                setTimeSelected((before) => ({
+                  ...before,
+                  fullTime:
+                    timeFormat === '12h'
+                      ? `${before.fullTime?.split(' ')[0]} ${opt}`
+                      : before.fullTime,
+                  meridian: timeFormat === '12h' ? opt : 'am',
+                }));
+                onTimeSelected({
+                  ...timeSelected,
+                  fullTime:
+                    timeFormat === '12h'
+                      ? `${timeSelected.fullTime?.split(' ')[0]} ${opt}`
+                      : timeSelected.fullTime,
+                  meridian: timeFormat === '12h' ? opt : 'am',
+                });
+                setZone(opt);
+              }}
+            >
+              <Paragraph
+                align='center'
+                size='sm'
+                color={zone === opt ? colors[shapeColor].text : 'dark'}
+              >
+                {opt}
+              </Paragraph>
+            </Option>
+          ))}
+        </Container>
+      )}
+    </StyledTimePicker>
+  );
+};
+
+export default TimePicker;
